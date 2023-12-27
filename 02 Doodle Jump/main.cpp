c17c2a0f355e02060877f6523d5228cbeca1b843
AHMED RAYEN <131598929+rayen-feb@users.noreply.github.com>
2023-12-27 03:59:57 -0500

    Update main.cpp
    separer les plateform verticalement 
    separer les plateformes  horizantallemenr 
    ajouter de song

--- 02 Doodle Jump/main.cpp
+++ 02 Doodle Jump/main.cpp
@@ -1,7 +1,11 @@
 #include <SFML/Graphics.hpp>
 #include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
 using namespace sf;
-struct perso{ int e;};
+
 struct point
 { int x,y;};
 
@@ -20,14 +24,41 @@ int main()
     Sprite sBackground(t1), sPlat(t2), sPers(t3);
 
     point plat[20];
+     int j,j1 ; 
+    //  posiont des plateformes 
 
+  
     for (int i=0;i<10;i++)
-      {
-       plat[i].x=rand()%400;
-       plat[i].y=rand()%533;
+
+     {
+          //  separation de plateform paraport a y  par distance de 50  
+        int temp = rand() % 515;  //  515 = 533-hauteur  de plateform=14 
+        for( j = 0; j < i; j++) {
+            if(abs(plat[j].y - temp) < 50  ) {
+                break;
+            }
+        }
+        if(j == i) {
+            plat[i].y = temp;
+            i++;}
+            // separation de plateformes paraport a x par une distance de 100
+int temp1 = rand() % 342;  // 342 = 400 -68
+        for(int  j1 = 0; j1 < i; j1++) {
+            if(abs(plat[j].x - temp1) <100 ) {         break;
+            }
+        }
+        if(j == i) {
+            plat[i].x = temp;
+            i++;}
+        
+
+        
+
+       plat[i].x=rand()%342;
+       plat[i].y=rand()%515;
       }
 
-	int x=100,y=100,h=200;
+        int x=100,y=100,h=300;
     float dx=0,dy=0;
 
     while (app.isOpen())
@@ -38,30 +69,35 @@ int main()
             if (e.type == Event::Closed)
                 app.close();
         }
+   // controles 
 
     if (Keyboard::isKeyPressed(Keyboard::Right)) x+=3;
     if (Keyboard::isKeyPressed(Keyboard::Left)) x-=3;
 
     dy+=0.2;
     y+=dy;
-    if (y>500)  dy=-10;
+ 
+   //  reglage de position de  plateform 
+    if (y>500)  dy=-10;  //  gravitee    (perso tombe si y >500 ) 
 
-	if (y<h)
+        if (y<h)
     for (int i=0;i<10;i++)
     {
       y=h;
       plat[i].y=plat[i].y-dy;
-      if (plat[i].y>533) {plat[i].y=0; plat[i].x=rand()%400;}
+      if (plat[i].y>514) {plat[i].y=0; plat[i].x=rand()%350;}
     }
+  //  reglage de position de personage  
 
-	for (int i=0;i<10;i++)
+        for (int i=0;i<10;i++)
     if ((x+50>plat[i].x) && (x+20<plat[i].x+68)
     && (y+70>plat[i].y) && (y+70<plat[i].y+14) && (dy>0))  dy=-10;
 
-	sPers.setPosition(x,y);
+        sPers.setPosition(x,y);
 
     app.draw(sBackground);
     app.draw(sPers);
+
     for (int i=0;i<10;i++)
     {
     sPlat.setPosition(plat[i].x,plat[i].y);
@@ -73,3 +109,4 @@ int main()
 
     return 0;
 }
+
