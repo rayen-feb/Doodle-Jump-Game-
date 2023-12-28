2544d73da838dcb9f2d82bd02010e3dd095fe380
AHMED RAYEN <131598929+rayen-feb@users.noreply.github.com>
2023-12-28 17:06:57 -0500

    Update main.cpp
      condition lorsque  preso touche le ground  ( dy  controle la vitesse de saut ) ; 
                 // ajouter une  popup  game over
                  //  faire revenir au menu prinicpa
    $

--- 02 Doodle Jump/main.cpp
+++ 02 Doodle Jump/main.cpp
@@ -1,34 +1,12 @@
-#include <SFML/Graphics.hpp>
-#include <time.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-
-using namespace sf;
-
-struct point
-{ int x,y;};
-
-int main()
-{
-    srand(time(0));
-
-    RenderWindow app(VideoMode(400, 533), "Doodle Game!");// name windo = doodle jump  ; resolution 44**533 
-    app.setFramerateLimit(60);  //  set the frame 60 
-
-    Texture t1,t2,t3; 
-    t1.loadFromFile("images/background.png");///////////////////////
-    t2.loadFromFile("images/platform.png");/////////////////////////////
-    t3.loadFromFile("images/doodle.png");////////////////////////
-
-    Sprite sBackground(t1), sPlat(t2), sPers(t3);
+  GNU nano 6.4                                                                                                                                                                                                                                                                                                                                                                                                                                                                         main.cpp *                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
 
     point plat[20];
      int j,j1 ; 
     //  posiont des plateformes 
 
   
-    for (int i=0;i<10;i++)
+  gameOver();
+  for (int i=0;i<10;i++)
 
      {
           //  separation de plateform paraport a y  par distance de 50  
@@ -44,12 +22,14 @@ int main()
             // separation de plateformes paraport a x par une distance de 100
 int temp1 = rand() % 342;  // 342 = 400 -68
         for(int  j1 = 0; j1 < i; j1++) {
-            if(abs(plat[j].x - temp1) <100 ) {         break;
+            if(abs(plat[j].x - temp1) <100 ) {
+                break;
             }
         }
         if(j == i) {
             plat[i].x = temp;
             i++;}
+
         
 
         
@@ -59,7 +39,7 @@ int temp1 = rand() % 342;  // 342 = 400 -68
       }
 
         int x=100,y=100,h=300;
-    float dx=0,dy=0;
+    float dy1=0, dx=0,dy=0;
 
     while (app.isOpen())
     {
@@ -74,24 +54,31 @@ int temp1 = rand() % 342;  // 342 = 400 -68
     if (Keyboard::isKeyPressed(Keyboard::Right)) x+=3;
     if (Keyboard::isKeyPressed(Keyboard::Left)) x-=3;
 
-    dy+=0.2;
+    dy+=0.15; //  // vitesse      controle la longeure de sot 
     y+=dy;
  
+
+
    //  reglage de position de  plateform 
-    if (y>500)  dy=-10;  //  gravitee    (perso tombe si y >500 ) 
+
+    if (y>500)   dy=-10;  //   condition lorsque  preso touche le ground  ( dy  controle la vitesse de saut ) ; 
+             // ajouter une  popup  game over
+              //  faire revenir au menu prinicpale   
 
         if (y<h)
     for (int i=0;i<10;i++)
     {
       y=h;
       plat[i].y=plat[i].y-dy;
-      if (plat[i].y>514) {plat[i].y=0; plat[i].x=rand()%350;}
+      if (plat[i].y>514) {plat[i].y=0; plat[i].x=rand()%350;}  // initialisation de position des  plate  lorsquils arrive au top  
     }
+   
   //  reglage de position de personage  
 
         for (int i=0;i<10;i++)
     if ((x+50>plat[i].x) && (x+20<plat[i].x+68)
-    && (y+70>plat[i].y) && (y+70<plat[i].y+14) && (dy>0))  dy=-10;
+    && (y+70>plat[i].y) && (y+70<plat[i].y+14) && (dy>0)) // condition lorsque le personnage touge la plateforme 
+         dy=-10;
 
         sPers.setPosition(x,y);
 
@@ -109,4 +96,11 @@ int temp1 = rand() % 342;  // 342 = 400 -68
 
     return 0;
 }
+
+
+
+
+
+
+
 
